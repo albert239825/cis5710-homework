90ad077890fa04381078bad47f89dec186f43e33
Albert <albert23982@gmail.com>
2026-01-28 17:27:14 +0000

    fixed syntax for 1 iter and implemented divider

--- hw2a-divider/DividerUnsigned.sv
+++ hw2a-divider/DividerUnsigned.sv
@@ -13,6 +13,33 @@ module DividerUnsigned (
 
     // TODO: your code here
 
+    wire [31:0] remainder[0:32];
+    wire [31:0] quotient[0:32];
+    wire [31:0] dividend[0:32];
+
+    assign remainder[0] = 32'd0;
+    assign quotient[0]  = 32'd0;
+    assign dividend[0]  = i_dividend;
+
+    genvar i;
+    generate
+        for (i = 0; i < 32; i = i + 1) begin : div_one_iter_loop
+            DividerOneIter div_one_iter(
+                .i_dividend(dividend[i]),
+                .i_divisor(i_divisor),
+                .i_remainder(remainder[i]),
+                .i_quotient(quotient[i]),
+                .o_dividend(dividend[i+1]),
+                .o_remainder(remainder[i+1]),
+                .o_quotient(quotient[i+1])
+            );
+        end
+    endgenerate
+
+    assign o_remainder = remainder[32];
+    assign o_quotient = quotient[32];
+    
+
 endmodule
 
 
@@ -43,15 +70,15 @@ module DividerOneIter (
     logic [31:0] rem_shift;
 
     always_comb begin
-        rem_shift = i_remainder << 1 | {31'b0, i_dividend[31]};
+        rem_shift = {i_remainder[30:0], i_dividend[31]};
         if (rem_shift < i_divisor) begin
             o_quotient = i_quotient << 1;
             o_remainder = rem_shift;
         end else begin
-            o_quotient = {i_quotient[31:1], 1'b1};
+            o_quotient = {i_quotient[30:0], 1'b1};
             o_remainder = rem_shift - i_divisor;
         end
-        o_dividend = {i_dividend[31:1], 1'b0};
+        o_dividend = {i_dividend[30:0], 1'b0};
     end
 
 endmodule
